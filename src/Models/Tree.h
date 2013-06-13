ce3ee69a192f020a6081bf52e2e418243cf17489
Rahul Shardha <rahul.shardha@gmail.com>
2013-06-13 19:40:20 -0400

    Added implementations for the constructor and search function declarations

--- src/Models/Tree.h
+++ src/Models/Tree.h
@@ -8,9 +8,13 @@
 #ifndef TREE_H_
 #define TREE_H_
 
+#include <string>
+using namespace std;
+
 class treeNode {
 	bool treeNode();
 	bool treeNode(Data* data);
+	bool treeNode(treeNode* left, treeNode* right, Data* data);
 
 	treeNode* getLeftChild(){
 		return mLeftChild;
@@ -21,31 +25,47 @@ class treeNode {
 	}
 
 	bool insertNode (treeNode* node);
-	bool deleteNode (); // deletes the current node
-	bool deleteNode (int identifier); //finds and deletes the node with the certain identifier
-	int generateIdentifier();
+	bool removeNode (); // deletes the current node
+	bool removeNode (int identifier); //finds and deletes the node with the certain identifier
+	static treeNode* searchNode();
+	static treeNode* searchNode(int key);
 
-	void setLeftChild(treeNode* child){
-		mLeftChild = new treeNode();
-		mLeftChild = child;
-	}
-
-	void setRightChild(treeNode* child){
-		mRightChild = new treeNode();
-		mRightChild = child;
-	}
+	static int generateKey(); //Dependant on data
 
 
 private:
-	Data* data;
+	Data* mData;
 	treeNode* mLeftChild;
 	treeNode* mRightChild;
 
-	int mIdentifier;
+	int mKey;//Dependant on data .. could be size ... student numbers ... lenght of string etc ....
+
+	void initializeChildrenToNullAndKey();
 };
 
+void treeNode::initializeChildrenToNullAndKey() {
+	this->mLeftChild = nullptr;
+	this->mRightChild = nullptr;
+	this->mKey = generateKey();
+}
+
 bool treeNode::treeNode(){
-	treeNode* currNode = new treeNode;
+	initializeChildrenToNullAndKey();
+	this->mData = nullptr;
+	return true;
+}
+
+bool treeNode::treeNode(Data* data){
+	initializeChildrenToNullAndKey();
+	this->mData = data;
+	return true;
+}
+
+bool treeNode::treeNode(treeNode* left, treeNode* right, Data* data){
+	this->mLeftChild = left;
+	this->mRightChild = right;
+	this->mData = data;
+	this->mKey = generateKey();
 	return true;
 }
 
